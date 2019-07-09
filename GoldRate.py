defe7c429a8a879cb228f9499bb0dd473ed97d1b
soumilshah1995 <soushah@my.bridgeport.edu>
2019-07-08 22:43:47 -0400

    updates

---
+++ GoldRate.py
@@ -0,0 +1,20 @@
+import  requests
+
+
+headers = {
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Referer': 'http://www.wikipedia.org/',
+    'Connection': 'keep-alive',
+}
+
+
+url = " https://data-asg.goldprice.org/dbXRates/USD"
+
+r = requests.get(url=url, headers=headers)
+data = r.json()
+
+gold_price = data["items"][0]["xauPrice"]
+print(gold_price)
